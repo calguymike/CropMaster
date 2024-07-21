1370214fd9786b8e1ad1194af476690df418e8b9
FarmingEngineerMike <mike@hallfarming.co.uk>
2024-07-21 20:10:16 +0100

    More development

--- CropMaster.py
+++ CropMaster.py
@@ -3,6 +3,7 @@ import streamlit_authenticator as stauth
 from pymongo import MongoClient
 import certifi
 import streamlit_authenticator as stauth
+from streamlit_authenticator.utilities.hasher import Hasher
 
 #Connect to database and initialise collections
 uri = st.secrets.mongo.uri
@@ -16,7 +17,37 @@ user_db = client.Users
 users_collection = user_db.Credentials
 
 def load_content():
-    st.title("Welcome to Hall Farming")
+    tab1, tab2 = st.tabs(["Home", "User Settings"])
+    with tab1:
+        st.title("Welcome to Hall Farming")
+        st.write(f'Welcome *{st.session_state["name"]}*')
+
+    with tab2:
+        try:
+            with st.form("Set New Password"):
+                new_password = st.text_input("New Password", type="password")
+                confirm_password = st.text_input("Confirm New Password", type="password")
+                submit_button = st.form_submit_button("Submit")
+
+                if submit_button:
+                    username = st.session_state["username"]
+                    if new_password != confirm_password:
+                        st.error("Passwords do not match")
+                    elif new_password and confirm_password:
+                        hashed_password = Hasher([new_password]).generate()[0]
+                        filter = {f'usernames.{username}': {'$exists': True}}
+                        update = {'$set': {f'usernames.{username}.password': hashed_password}}
+                        update_result = users_collection.update_one(filter, update)
+                    else:
+                        st.warning("Please enter a new password")
+                    if update_result.modified_count > 0:
+                        st.success("Password modified successfully")
+                    else:
+                        st.error("Failed to update the password")
+                else:
+                    st.warning("Please enter a new password")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
 def authenticate_user():
     credentials = users_collection.find_one({}, {"_id": 0, "usernames": 1})
@@ -33,16 +64,11 @@ def authenticate_user():
         pre_auth
     )
     authenticator.login()
+    
 
     if st.session_state["authentication_status"]:
         authenticator.logout()
         st.write(f'Welcome *{st.session_state["name"]}*')
-        # Reset Password
-        try:
-            if authenticator.reset_password(st.session_state["username"]):
-                st.success('Password modified successfully')
-        except Exception as e:
-            st.error(e)
         # Load app
         load_content()
     elif st.session_state["authentication_status"] is False:
--- pages/Cropping.py
+++ pages/Cropping.py
@@ -4,6 +4,8 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit_authenticator as stauth
+from datetime import datetime
+from bson import ObjectId
 
 
 #Connect to database and initialise collections
@@ -14,20 +16,47 @@ client = MongoClient(uri, tlsCAFile=certifi.where())
 db = client.HallFarming
 fields_collection = db.Land
 crops_collection = db.Crops
+dressings_collection = db.Dressings
+varieties_collection = db.Varieties
 user_db = client.Users
 users_collection = user_db.Credentials
 
+def get_crop(field):
+    cropping = field.get("Cropping", {}).get("Crops", [])
+    if cropping:
+        latest_crop = cropping[-1]
+        crop = latest_crop.get("Crop", "")
+        dressing = latest_crop.get("Dressing", "")
+        drill_date = latest_crop.get("DrillDate", "").date() if latest_crop.get("DrillDate", "") else ""
+        cut_date = latest_crop.get("CutDate", "").date() if latest_crop.get("CutDate", "") else ""
+        home_saved = latest_crop.get("HomeSaved", "")
+        quantity = latest_crop.get("Quantity", "")
+        variety = latest_crop.get("Variety", "")
+        crop_yield = latest_crop.get("Yield", "")
+    else:
+        crop = dressing = drill_date = cut_date = home_saved = quantity = variety = crop_yield = ""
+    
+    return {
+        "Crop": crop,
+        "Dressing": dressing,
+        "DrillDate": drill_date,
+        "CutDate": cut_date,
+        "HomeSaved": home_saved,
+        "Quantity": quantity,
+        "Variety": variety,
+        "Yield": crop_yield
+    }
 
 def load_content():
     # Set tabs at top of page
-    tab1, tab2, tab3 = st.tabs(["Manage Crops", "Manage Fields", "Settings"])
+    tab1, tab2, tab3 = st.tabs(["Field Cropping", "Manage Crops & Fields", "Settings"])
 
     # Manage Crops tab
     with tab1:
         st.title("Manage crops")
         # Fetch distinct farms
         farms = fields_collection.distinct("Farm")
-        selected_farm = st.radio("Select Farm", farms)
+        selected_farm = st.selectbox("Select Farm", farms)
 
         if selected_farm:
             # Fetch fields for the selected farm
@@ -38,31 +67,15 @@ def load_content():
             for field in fields:
                 field_name = field["FieldName"]
                 size = field.get("Size", "")
-                cropping = field.get("Cropping", {}).get("Crops", [])
-                if cropping:
-                    latest_crop = cropping[-1]
-                    crop = latest_crop.get("Crop", "")
-                    dressing = latest_crop.get("Dressing", "")
-                    drill_date = latest_crop.get("DrillDate", "")
-                    origin = latest_crop.get("Origin", "")
-                    quantity = latest_crop.get("Quantity", "")
-                    variety = latest_crop.get("Variety", "")
-                else:
-                    crop = dressing = drill_date = origin = quantity = variety = ""
+                latest_crop = get_crop(field)
                 data.append({
                     "FieldName": field_name,
                     "Size": size,
-                    "Crop": crop,
-                    "Dressing": dressing,
-                    "DrillDate": drill_date,
-                    "Origin": origin,
-                    "Quantity": quantity,
-                    "Variety": variety
-                })
+                    **latest_crop})
 
             # Display data in a table
             df = pd.DataFrame(data)
-            st.dataframe(df)
+            #st.dataframe(df)
 
 
             # Prepare data for the Sankey diagram for the selected farm
@@ -95,43 +108,69 @@ def load_content():
                     value=values
                 ))])
 
-            fig.update_layout(title_text=f"Crop Distribution for {selected_farm}", font_size=10)
-            st.plotly_chart(fig)
+            tab4, tab5 = st.tabs(["Data View", "Chart View"])
+            with tab4:
+                st.dataframe(df)
 
+            with tab5:
+                fig.update_layout(title_text=f"Crop Distribution for {selected_farm}", font_size=10)
+                st.plotly_chart(fig)
 
             # Dropdown to select a specific field
             field_names = df["FieldName"].tolist()
             selected_field = st.selectbox("Select Field", field_names)
 
+            # Fetch crop data for the selected field
+            field = fields_collection.find_one({"FieldName": selected_field})
+            latest_crop = get_crop(field)
+
             # Fetch crop names from the Crops collection
             crop_names = crops_collection.distinct("Crop")
+            dressing_names = dressings_collection.distinct("Treatments")
 
             # Input fields for the new crop object
-            st.subheader("Add a New Crop")
+            st.subheader("Modify Cropping")     
 
             col1, col2 = st.columns(2)
             # Column 1
             with col1:
-                crop_name = st.selectbox("Crop Name", crop_names)
-                quantity = st.text_input("Quantity", "")
-                drill_date = st.date_input("Drill Date")
+                crop_name_mod = st.selectbox("Crop", crop_names)
+                quantity = st.number_input("Seed Quantity (kg)", step=5)
+                if quantity == 0:
+                    quantity = ""
+                variety = st.text_input("Variety", "")
+                drilled = st.toggle("Drilled")
+                if drilled:
+                    drill_date = datetime.combine(st.date_input("Drill Date"), datetime.min.time())
+                else:
+                    drill_date = ""
 
             # Column 2
             with col2:
                 origin = st.radio("Seed Source", ["Bought", "Home Saved"], horizontal=True)
-                dressing = st.text_input("Dressing", "")
-                variety = st.text_input("Variety", "")
-
-            # Button to submit the new crop object
+                home_saved = False
+                if origin == "Home Saved":
+                    home_saved = True
+                dressing = st.selectbox("Dressing", dressing_names)
+                crop_yield = st.number_input("Yield (ton/ha)", step=5)
+                harvested = st.toggle("Harvested")
+                if harvested:
+                    cut_date = datetime.combine(st.date_input("Cut Date"), datetime.min.time())
+                else:
+                    cut_date = ""
+                
+        
             if st.button("Add Crop"):
                 # Define the new crop object
                 new_crop = {
-                    "Crop": crop_name,
+                    "Crop": crop_name_mod,
                     "Dressing": dressing,
                     "DrillDate": drill_date,
-                    "Origin": origin,
+                    "CutDate": cut_date,
+                    "HomeSaved": home_saved,
                     "Quantity": quantity,
-                    "Variety": variety
+                    "Variety": variety,
+                    "Yield": crop_yield
                 }
 
                 # Update the selected document
@@ -140,11 +179,41 @@ def load_content():
                     {"$push": {"Cropping.Crops": new_crop}}
                 )
 
-                st.success(f"Crops added successfully to {selected_field}!")
+                st.success(f"Crop added successfully to {selected_field}!")       
 
     # Manage Fields tab
     with tab2:
-        st.title("Manage Fields")
+        # Fetch data from MongoDB
+        def fetch_data():
+            crops = list(crops_collection.find({}))
+            return crops
+
+        # Update data in MongoDB
+        def update_data(data, ids):
+            for index, row in data.iterrows():
+                filter = {"_id": ids[index]}
+                new_values = {"$set": {"Crop": row["Crop"], "identifier": row["identifier"], "N_Max": row["N_Max"], "P_Max": row["P_Max"]}}
+                crops_collection.update_one(filter, new_values)
+
+        # Load data into a DataFrame
+        crops = fetch_data()
+        df = pd.DataFrame(crops)
+
+        # Keep track of _id values separately
+        id_list = df["_id"].apply(str).tolist()
+        df = df.drop(columns=["_id"])  # Drop the _id column for display
+
+        st.title("Edit Crop Information")
+        st.write("Edit the table below and click 'Save Changes' to update the data in the database.")
+
+        # Display editable table with a unique key
+        edited_df = st.data_editor(df, key='data_editor')
+
+        # Button to save changes
+        if st.button("Save Changes"):
+            # Ensure the order of ids matches the edited_df
+            update_data(edited_df, [ObjectId(id_str) for id_str in id_list])
+            st.success("Data updated successfully!")
 
 
     with tab3:
@@ -153,9 +222,7 @@ def load_content():
         if st.button("Delete all crops from all fields"):
             # Update all documents to remove the Cropping field
             result = fields_collection.update_many({}, {"$unset": {"Cropping": ""}})
-
             st.success(f"Deleted {result.modified_count} documents!")
-    client.close()
 
 def authenticate_user():
     credentials = users_collection.find_one({}, {"_id": 0, "usernames": 1})
@@ -172,6 +239,7 @@ def authenticate_user():
         pre_auth
     )
     authenticator.login()
+    
 
     if st.session_state["authentication_status"]:
         authenticator.logout()
